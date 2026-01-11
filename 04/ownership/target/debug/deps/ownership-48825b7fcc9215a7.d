7a66fb800733e3e7c2aa20564d76b796e44341f9
santostebota <162834964+santosksl@users.noreply.github.com>
2026-01-10 23:50:12 -0300

    new

---
+++ 04/ownership/target/debug/deps/ownership-48825b7fcc9215a7.d
@@ -0,0 +1,5 @@
+C:\Users\ssant\OneDrive\Documentos\Rust\04\ownership\target\debug\deps\ownership-48825b7fcc9215a7.d: src\main.rs
+
+C:\Users\ssant\OneDrive\Documentos\Rust\04\ownership\target\debug\deps\ownership-48825b7fcc9215a7.exe: src\main.rs
+
+src\main.rs:
---
+++ 04/ownership/target/debug/ownership.d
@@ -0,0 +1,1 @@
+C:\Users\ssant\OneDrive\Documentos\Rust\04\ownership\target\debug\ownership.exe: C:\Users\ssant\OneDrive\Documentos\Rust\04\ownership\src\main.rs
