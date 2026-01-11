4e60cebddbcd6c395d5a523c21c56248ea105ba9
santostebota <162834964+santosksl@users.noreply.github.com>
2026-01-11 17:12:40 -0300

    new

--- 04/ownership/src/main.rs
+++ 04/ownership/src/main.rs
@@ -1,15 +1,30 @@
 fn main() {
+    /*
+        gives_ownership move seu retorno
+        valor em s1
+    */
+
     let s1 = gives_ownership();
+
+    //
+
+    /*
+        s2 entra no escopo;
+        s2 é movido para;
+        takes_and_gives_back que tambem move seu valor de retorno para s3;
+
+    */
+
     let s2 = String::from("hello");
     let s3 = takes_and_gives_back(s2);
 
     println!("{s1}");
     println!("{s3}");
-}
+} //s3 sai do escopo entao é .drop
+//s1 sai do escopo entao é .drop
 
 fn gives_ownership() -> String {
     let some_string = String::from("yours");
-
     some_string
 }
 
